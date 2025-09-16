51f6e3dfcde67d538a588c44bb59b967861ce222
ritikj0536 <124515293+ritikj0536@users.noreply.github.com>
2025-09-16 19:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    // Min heap using priority queue
+    const minHeap = [];
+    
+    // Build the heap
+    for (let num of arr) {
+        minHeap.push(num);
+    }
+    minHeap.sort((a, b) => a - b); // sort ascending
+
+    let cost = 0;
+
+    while (minHeap.length > 1) {
+        // Take two smallest ropes
+        let first = minHeap.shift();
+        let second = minHeap.shift();
+
+        let newRope = first + second;
+        cost += newRope;
+
+        // Insert new rope in sorted order
+        let i = 0;
+        while (i < minHeap.length && minHeap[i] < newRope) {
+            i++;
+        }
+        minHeap.splice(i, 0, newRope);
+    }
+
+    return cost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
